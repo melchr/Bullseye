284bda443ab2cff5dc56ae68904028dcd6f9292f
Mel C <melchrstmn@gmail.com>
2023-03-05 22:10:04 -0500

    simplified code in game file

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -13,6 +13,15 @@ struct Game {
   var round: Int = 1
   
   func points(sliderValue: Int) -> Int {
-    return 999
+    var difference: Int = target - sliderValue
+    if difference < 0 {
+      difference = difference * -1
+      // or difference *= -1
+      //or difference = -difference
+    }
+
+    
+    var awardedPoints: Int = 100 - difference
+    return awardedPoints
   }
 }
