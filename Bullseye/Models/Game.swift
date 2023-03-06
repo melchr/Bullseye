ea0a391e8b9653bd8b56c0a015b3115c4bc316e7
Mel C <melchrstmn@gmail.com>
2023-03-05 22:17:32 -0500

    changed some var variables to let

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -6,7 +6,7 @@ struct Game {
   var round: Int = 1
   
   func points(sliderValue: Int) -> Int {
-    var difference: Int = abs(target - sliderValue)
+    let difference: Int = abs(target - sliderValue)
     //if difference < 0 {
      // difference = difference * -1
       // or difference *= -1
@@ -14,7 +14,7 @@ struct Game {
    // }
 
     
-    var awardedPoints: Int = 100 - difference
+    let awardedPoints: Int = 100 - difference
     return awardedPoints
   }
 }
--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
           }
         },
         message: {
-          var roundedValue: Int = Int(sliderValue.rounded())
+          let roundedValue: Int = Int(sliderValue.rounded())
           Text("""
             The slider's value is \(roundedValue).
             You scored \(game.points(sliderValue: roundedValue)) points this round.
