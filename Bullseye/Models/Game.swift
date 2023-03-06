31941a39760f594e071b9a11758e1c1aec171994
Mel C <melchrstmn@gmail.com>
2023-03-05 22:13:48 -0500

    cleaned up code further in game file, added abs function to perform score calculation

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -1,10 +1,3 @@
-//
-//  Game.swift
-//  Bullseye
-//
-//  Created by Mel C on 3/5/23.
-//
-
 import Foundation
 
 struct Game {
@@ -13,12 +6,12 @@ struct Game {
   var round: Int = 1
   
   func points(sliderValue: Int) -> Int {
-    var difference: Int = target - sliderValue
-    if difference < 0 {
-      difference = difference * -1
+    var difference: Int = abs(target - sliderValue)
+    //if difference < 0 {
+     // difference = difference * -1
       // or difference *= -1
       //or difference = -difference
-    }
+   // }
 
     
     var awardedPoints: Int = 100 - difference
