6bad31f84e1e571297560b08624522110018f878
Mel C <melchrstmn@gmail.com>
2023-03-06 20:32:57 -0500

    added early dark mode styling

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -79,5 +79,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+    ContentView()
+      .preferredColorScheme(.dark)
+      .previewDevice("iPhone 14 Pro Max")
   }
 }
