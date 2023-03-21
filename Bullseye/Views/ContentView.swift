ead8593441335c84129349c19da4daffbe001be6
Mel C <melchrstmn@gmail.com>
2023-03-20 22:39:14 -0400

    centered the slider

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -17,9 +17,10 @@ struct ContentView: View {
       BackgroundView(game: $game)
       VStack {
         InstructionsView(game: $game)
-        SliderView(sliderValue: $sliderValue)
+          .padding(.bottom, 100)
         HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
       }
+      SliderView(sliderValue: $sliderValue)
     }
   }
 }
