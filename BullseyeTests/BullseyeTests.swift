667e92f76a401bb870b06b93dd5a2033535f22d3
Mel C <melchrstmn@gmail.com>
2023-03-20 22:00:29 -0400

    added 2 new tests

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -23,6 +23,18 @@ final class BullseyeTests: XCTestCase {
     XCTAssertEqual(score, 95)
   }
   
+  func testScoreExact() {
+    let guess = game.target
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 200)
+  }
+  
+  func testScoreClose() {
+    let guess = game.target + 2
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 98 + 50)
+  }
+  
   func testNewRound() {
     game.startNewRound(points: 100)
     XCTAssertEqual(game.score, 100)
