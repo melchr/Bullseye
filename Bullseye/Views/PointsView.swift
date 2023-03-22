43535e7ed26648af17d2af3cccc3479daa0b8d91
Mel C <melchrstmn@gmail.com>
2023-03-22 09:41:28 -0400

    added new view PointsView

---
+++ Bullseye/Views/PointsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PointsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PointsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PointsView()
+    }
+}
